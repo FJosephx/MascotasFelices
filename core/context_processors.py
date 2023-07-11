c865e7c1dd90622d1685823aa4c374647d2fbdc0
Luis <luisignaciocontacto@gmail.com>
2023-07-11 12:27:31 -0300

    v5.2

--- core/context_processors.py
+++ core/context_processors.py
@@ -5,10 +5,7 @@ def registrar_carrito(request):
     mostrar_carrito = False
     cantidad_productos = 0
     
-    if request.user.is_authenticated:
-        if request.user.perfil.tipo_usuario == 'Cliente':
-            cantidad_productos = Carrito.objects.filter(cliente=request.user.perfil).count()
-            mostrar_carrito = cantidad_productos > 0
+
 
         
    
